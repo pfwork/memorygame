13f39242bbf4bbfbb031bdef4a9fae6f7d9d6e93
Pei Fan <yellowhite5@gmail.com>
2018-08-07 09:34:26 -0500

    Add modal box, time recorder.
    Signed-off-by: Pei Fan <yellowhite5@gmail.com>

--- js/app.js
+++ js/app.js
@@ -4,20 +4,34 @@
  // This array holds the currently clicked cards
 let openCardsList = [];
 
+let counter = 0;
+
+let matchedCardsCount = 0;
+
+let seconds = 0;
+
+var startTime, endTime;
+
+var timeOut;
+
+newDeck();
 
 // Add a eventlistener to the restart button
 document.getElementById('repeat').addEventListener('click', newDeck);
 
 function newDeck() {
-  const cards = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor",
-              "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle",
-              "fa fa-bomb", "fa fa-bomb"];
 
+  let cards = [];
+
+  let findI = document.querySelectorAll(".deck i");
+
+  for(i = 0; i < findI.length; i++) {
+    cards[i] = findI[i].className;
+  }
   //Shuffle cards
   const newCards = shuffle(cards);
 
   //Put shuffled cards on the screen
-  let findI = document.querySelectorAll(".deck i");
   for (i = 0; i < findI.length; i++) {
     findI[i].className = cards[i];
   }
@@ -28,6 +42,11 @@ function newDeck() {
   for (i = 0; i < myList.length; i++) {
     myList[i].className = "card";
   }
+
+  // Clear open cards list when restart button is clicked
+  openCardsList.length = 0;
+  counter = 0;
+  document.querySelector(".moves").textContent = counter;
 }
 
 // Make each card on the deck clickable
@@ -35,45 +54,43 @@ document.getElementById('cardDeck').addEventListener('click', displayCard);
 
 // When a card is clicked, display it on the deck
 function displayCard(evt) {
-  console.log("DisplayCard called");
+  console.log("displayCard function is called");
   let classNames = evt.target.classList;
-  console.log("clicked cards classes: " + classNames);
 
-  if (evt.target.nodeName === 'LI') {
-
-  // If a card is clicked multiple times, only one click is counted
-  if (!(classNames.contains("open"))) {
+  if ((evt.target.nodeName === 'LI') && (openCardsList.length < 2)){
+    // If a card is clicked multiple times, only one click is counted
+    if (!(classNames.contains("open", 'show'))) {
+      counter += 1;
       classNames.add('open', 'show');
+      document.querySelector(".moves").textContent = counter;
       openCards(evt.target.id);
     }
+  }
+
+  if (counter === 1) {
+    startTimer();
+    console.log("Timer started");
   }
 }
 
 // When a card is clicked, push it's id into an array
 function openCards(id) {
   console.log("openCards called on id: " + id);
-  // Do I really need array
 
   openCardsList.push(id);
   if(openCardsList.length === 2) {
     checkMatch();
   }
-  // if (oc.length == 0) {
-    // oc.push(id);
-  // }
-  // else {
-    // checkMatch(id);
-// }
 
+  console.log("openCardsList array has: " + openCardsList[0] + " " + openCardsList[1]);
   console.log(openCardsList);
-  // return oc;
+
 }
 
 // This function is to check if the two opend cards match
 function checkMatch() {
 
   console.log("checkMatch() function is called");
-
   var cardOneClassName = document.getElementById(openCardsList[0]).firstElementChild.className;
   var cardTwoClassName = document.getElementById(openCardsList[1]).firstElementChild.className;
   console.log("OC array has: " + cardOneClassName + " " + cardTwoClassName);
@@ -83,8 +100,6 @@ function checkMatch() {
       console.log('you got it!');
 
       matchedCards();
-
-
     }
   else {
       console.log('oh, no!');
@@ -100,17 +115,23 @@ function checkMatch() {
 
 // if two cards match, stay in the match position
 function matchedCards() {
-  console.log("Before matchedCards called" + document.getElementById(openCardsList[0]).className);
-  document.getElementById(openCardsList[0]).classList.add('match');
-  document.getElementById(openCardsList[0]).classList.remove('open', 'show');
 
-  document.getElementById(openCardsList[1]).classList.add('match');
-  document.getElementById(openCardsList[1]).classList.remove('open', 'show');
-  console.log("AftermatchedCards called" + document.getElementById(openCardsList[0]).className);
+  for(i = 0; i < openCardsList.length; i++) {
+    document.getElementById(openCardsList[i]).classList.add('match');
+    document.getElementById(openCardsList[i]).classList.remove('open', 'show');
+  }
+
   openCardsList.length=0;
+  matchedCardsCount += 2;
+  console.log("matched cards are now " + matchedCardsCount);
+  if (matchedCardsCount === 16) {
+    myModal();
+    stopTimer();
+    console.log("Time elapsed " + seconds);
+  }
 }
 
-// If two cards don't match, hide cards simbol 
+// If two cards don't match, hide cards simbol
 function closeCards()
 {
   console.log("close cards function called");
@@ -119,11 +140,38 @@ function closeCards()
   openCardsList.length = 0;
 }
 
+// Modal box function
+function myModal() {
+  // Get the modal
+  var modal = document.getElementById('myModal');
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
+  modal.style.display = "block";
 
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
 
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+  }
+}
 
-
+function startTimer() {
+  startTime = new Date();
+}
+function stopTimer() {
+  endTime = new Date();
+  var timeDiff = endTime - startTime;
+  timeDiff /= 1000;
+  seconds = Math.round(timeDiff);
+  console.log(seconds);
+}
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
